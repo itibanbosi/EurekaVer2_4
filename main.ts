04f7dc00bd9ed842b3682843f74e4d554898be1e
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-07-12 10:02:48 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -655,9 +655,11 @@ namespace eureka_blocks {
 
   //% color="#3943c6" weight=9 blockId=servos_backward
   //% block="後ろ" group="基本の動き"
+    pins.servoWritePin(AnalogPin.P14, 90+90*con_op/100 -20 + con_le);
+    pins.servoWritePin(AnalogPin.P13, 90-90*con_op/100 +20 + con_le);
+
   export function backward(): void {
-    pins.servoWritePin(AnalogPin.P14, 90+90*con_op -20 + con_le);
-    pins.servoWritePin(AnalogPin.P13, 90-90*con_op +20 + con_le);
+
   }
 
   //% color="#3943c6" weight=8 blockId=servos_left
