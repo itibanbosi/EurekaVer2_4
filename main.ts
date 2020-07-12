a45e8531426d4de45ef47c75b4edfd745afd430f
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-07-12 10:47:50 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -684,10 +684,10 @@ namespace eureka_blocks {
 
   //% color="#3943c6" weight=10　blockId=servo_pro_lr
   //% block="左右プログラム 左へ |%set_lr| 右へ" group="基本の動き"
-  //% set_lr.min=-50 set_lr.max=50
+  //% set_lr.min=-40 set_lr.max=40
   export function pro_lr(set_lr : number): void {
-    pins.servoWritePin(AnalogPin.P14, 90-90*(con_op+100)/100 + set_lr + con_le);
-    pins.servoWritePin(AnalogPin.P13, 90+90*(con_op+100)/100 + set_lr + con_le);
+    pins.servoWritePin(AnalogPin.P14, 90-50*(con_op+100)/100+set_lr + con_le);
+    pins.servoWritePin(AnalogPin.P13, 90+50*(con_op+100)/100+set_lr + con_le);
   }
 
 
