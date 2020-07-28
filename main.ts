fc99f1fdf6b36522a979223476b1ddf3fbd7a0bb
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2020-07-28 19:25:59 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -44,7 +44,7 @@ let con_op=0;
 //% color="#74ad1d" block="ﾕｰﾚｶﾌﾞﾛｯｸ2.4"
 namespace eureka_blocks {
 
-  //% color="#ff3d03" weight=59 blockId=eureka_buz_set block="ﾕｰﾚｶIOで音をならす" group="1_初期設定"
+  //% color="#ff3d03" weight=59 blockId=eureka_buz_set block="ﾕｰﾚｶIOﾎﾞｯｸｽで音をならす" group="1_初期設定"
   export function eureka_buz_set() {
     pins.analogSetPitchPin(AnalogPin.P8);
   }
